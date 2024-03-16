fbc396c64c04d07495597982cc4d79c91135582b
Mary Villegas <mavillegas0801@gmail.com>
2024-03-16 14:54:24 -0700

    add some plotting stuff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import brewtools
 import pysam 
-import matplotlib 
+import matplotlib.pyplot as plt
+import fastAreader
 
 class Main(self):
 	self.ref_genome = refGenome
@@ -8,7 +9,7 @@ class Main(self):
 
 	def GenCoverage(self):
 		posbp_read = {}
-		for i in range(1, len(ref_genome)+1)
+		for i in range(1, len(ref_genome)+1):
 			readCount = 0
 			for read in bamfile.fetch('ENA|MN908947|MN908947.3', i, i+1):
 				readCount += 1
@@ -21,7 +22,16 @@ class Main(self):
 		# graphing a histogram of the depth of the genome 
 		# x-axis is the base pair position
 		# y-axis is number of reads in that position
-
+		# Plotting a basic histogram
+        		plt.hist(i, readCount, bins=100, color='skyblue', edgecolor='black')
+ 
+        		# Adding labels and title
+        		plt.xlabel('Number of Reads')
+       		plt.ylabel('Nucleotide Position')
+        		plt.title('Read Depth of Genome')
+ 
+        		# Display the plot
+        		plt.show()
 	def main(self):
 		bamfile = pysam.AlignmentFile("alignment_1.bam", "rb")
 		refGenome = fastAreader(GCA...fasta)
